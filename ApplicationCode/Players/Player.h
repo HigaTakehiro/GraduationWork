7aa7047c34903bd372cefae9e4fc77791090e45d
Higa_0026 <k020g1382@g.neec.ac.jp>
2023-11-06 12:54:31 +0900

    成果 : アニメーションスピードを外部ファイルから変更可能にした、影モデル追加

--- ApplicationCode/Players/Player.h
+++ ApplicationCode/Players/Player.h
@@ -145,6 +145,11 @@ private: //メンバ関数
 	/// </summary>
 	void Repulsion();
 
+	/// <summary>
+	/// アニメーション処理
+	/// </summary>
+	void Animation();
+
 private: //メンバ変数
 	//座標
 	Vector3 pos_;
@@ -204,6 +209,10 @@ private: //メンバ変数
 	Model* rightMoveModel_[4];
 	//左
 	Model* leftMoveModel_[4];
+	//影オブジェクト
+	std::unique_ptr<Object3d> shadow_;
+	//影
+	Model* shadowModel_;
 
 	//ハンマーオブジェクト
 	std::unique_ptr<Object3d> hammer_;
@@ -229,6 +238,12 @@ private: //メンバ変数
 	Vector3 initHammerRot_;
 	//アニメーションカウント
 	int32_t animeCount_;
+	//アニメーションスピード
+	int32_t animeSpeed_;
+	//アニメーションタイマー
+	int32_t animeTimer_;
+	//前フレームアニメーションカウント
+	int32_t preAnimeCount_;
 
 	//矢印オブジェクト
 	std::unique_ptr<Object3d> arrow_;
