645ffe2e9716a7608befaa82457bfe2660d31ac5
matumonj <74886038+matumonj@users.noreply.github.com>
2023-11-09 19:03:18 +0900

    ぷる40WWWW

--- ApplicationCode/Scenes/GameScene.cpp
+++ ApplicationCode/Scenes/GameScene.cpp
@@ -245,7 +245,7 @@ void GameScene::Draw()
 	//テキスト描画範囲
 
 	D2D1_RECT_F textDrawRange = { 0, 0, 700, 700 };
-	std::wstring hp = std::to_wstring(enemys_.size());
+	std::wstring hp = std::to_wstring(player_->GetHP());
 	text_->Draw("meiryo", "white", L"ゲームシーン\n左クリックまたはLボタンでタイトルシーン\n右クリックまたはRボタンでリザルトシーン\nシェイクはEnter\nHP : " + hp, textDrawRange);
 
 	DirectXSetting::GetIns()->endDrawWithDirect2D();
