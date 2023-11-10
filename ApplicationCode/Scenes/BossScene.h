7880def1b9d271ad4792c496b6498bcbbb373052
shimadatuyoshi <k020g1186@g.neec.ac.jp>
2023-11-10 11:02:58 +0900

    ほれボスシーンだ使え、松本

---
+++ ApplicationCode/Scenes/BossScene.h
@@ -0,0 +1,86 @@
+#pragma once
+#include "BaseScene.h"
+#include "PostEffect.h"
+#include "SafeDelete.h"
+#include "MouseInput.h"
+#include "Object3d.h"
+#include "Vector3.h"
+#include "Camera.h"
+#include "MouseInput.h"
+#include "Easing.h"
+#include "Shapes.h"
+#include "ParticleManager.h"
+#include "LightGroup.h"
+#include "DebugText.h"
+#include "MotionMath.h"
+#include "SceneManager.h"
+#include "TextDraw.h"
+#include "JsonLoader.h"
+#include "Player.h"
+#include"GameMap.h"
+#include "Ore.h"
+#include"BossBase.h"
+#include"BaseEnemy.h"
+#include"Shake.h"
+
+class BossScene :
+    public BaseScene
+{
+public:
+    void Initialize();
+
+    void Update();
+
+    void Draw();
+
+    void Finalize();
+
+private:
+	/// <summary>
+	/// シーン切り替え処理
+	/// </summary>
+	void SceneChange();
+
+    /// <summary>
+    /// カメラ初期化設定
+    /// </summary>
+    void CameraSetting();
+
+private:
+	//ポストエフェクト
+	std::unique_ptr<PostEffect> postEffect_;
+	//ポストエフェクト番号
+	PostEffect::PostEffectNo postEffectNo_;
+	//ライト
+	std::unique_ptr<LightGroup> light_;
+	//カメラ
+	std::unique_ptr<Camera> camera_;
+	//プレイヤー
+	Player* player_;
+	//アイテム
+	std::unique_ptr<Ore> ore_;
+	//鉱石アイテム
+	std::list<std::unique_ptr<Ore>> oreItems_;
+
+	std::vector<BaseEnemy*> enemys_;
+	std::vector<Vector3> vec;
+	OBB* _hummmerObb;
+
+	std::unique_ptr<BossBase>boss_;
+	//テキスト描画
+	TextDraw* text_;
+	//仮地面
+	std::unique_ptr<GameMap> map_;
+	//背景画像
+	std::unique_ptr<Sprite> background_;
+	//カメラ座標
+	Vector3 cameraPos_;
+	//注視点
+	Vector3 targetPos_;
+
+	Shake* shake_;
+	float oldcamerapos_ = 0;
+	XMFLOAT3 nextPos_{};
+
+};
+
