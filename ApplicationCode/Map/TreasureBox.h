34a3248bc265b116a20a77fac6831545386a67b4
shimadatuyoshi <k020g1186@g.neec.ac.jp>
2023-12-20 23:09:18 +0900

    中身の作成

--- ApplicationCode/Map/TreasureBox.h
+++ ApplicationCode/Map/TreasureBox.h
@@ -1,5 +1,47 @@
 #pragma once
+#include"Object3d.h"
+#include<memory.h>
+#include<DirectXMath.h>
+using namespace std;
+using namespace DirectX;
+
+class Player;
 class TreasureBox
 {
+public:
+
+	void Initialize(bool& empmty, const XMFLOAT3& MapPos,Player* player,int Count);
+
+	void Update();
+
+	void Draw();
+
+	void Finalize();
+
+	bool GetLock() { return lock_; }
+
+	void SetLock(bool Lock) { this->lock_ = Lock; }
+
+private:
+
+	void CheckHit();
+
+private:
+	unique_ptr<Object3d> treasurebox_;
+	unique_ptr<Object3d> ui_;
+	unique_ptr<Object3d> key_;
+
+	Model* stairsModel_;
+
+	Model* uiModel_;
+
+	XMFLOAT3 pos_;
+	XMFLOAT3 uipos_;
+
+	Player* player_;
+
+	int count_ = 0;
+	bool lock_ = false;
+	bool f = false;
 };
 
